3b6a30ac381937d8d2613db18def50b4204886ec
John Taggart <taggartj1985@gmail.com>
2020-03-30 19:08:06 +0100

    fully working and background added

--- specs/game_specs.rb
+++ specs/game_specs.rb
@@ -5,20 +5,24 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 class GameTest< MiniTest::Test
 
-  def setup
-    @game1 = Game.new("player 1", "player 2")
-  end
+  # def setup
+  #   @game1 = Game.new("player 1", "player 2")
+  # end
 
-  def test_P1_wins()
-    assert_equal("Player 1 Wins", @game1.play("Paper", "Rock"))
-  end
+  # def test_P1_wins()
+  #   assert_equal("Player 1 Wins", @game1.check_win("Paper", "Rock"))
+  # end
 
   def test_P2_wins()
-    assert_equal("Player 2 Wins", @game1.play("Scissors", "Rock"))
+    assert_equal("Player 2 Wins", Game.play("scissors", "rock"))
   end
 
   def test_draw()
-    assert_equal("Draw", @game1.play("Paper", "Paper"))
+    assert_equal("Draw", Game.play("paper", "paper"))
+  end
+
+  def test_P1_wins()
+    assert_equal("Player 1 Wins", Game.play("paper", "rock"))
   end
 
 end
